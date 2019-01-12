b3095fde1fec370ab1f50776befe1e41fa747dce
Justin Lafferty <lafferjm@gmail.com>
2019-01-12 16:15:57 -0600

    add hero

--- src/components/Heroes.vue
+++ src/components/Heroes.vue
@@ -1,6 +1,16 @@
 <template>
   <div>
     <h2>My Heroes</h2>
+    
+    <div>
+      <label>Hero name:
+        <input v-model="heroName" />
+      </label>
+      <button v-on:click="add">
+        add
+      </button>
+    </div>
+
     <ul class="heroes">
       <li v-for="hero in heroes" v-bind:key="hero.id">
         <router-link v-bind:to="`/detail/${hero.id}`">
@@ -22,12 +32,22 @@ export default {
   },
   data: () => {
     return {
-      heroes: null
+      heroes: null,
+      heroName: ''
     };
   },
   mounted: async function() {
     const response = await axios.get('http://localhost:3000/heroes');
     this.heroes = response.data;
+  },
+  methods: {
+    add: async function() {
+      const response = await axios.post('http://localhost:3000/heroes', {
+        name: this.heroName
+      });
+      this.heroName = '';
+      this.heroes.push(response.data);
+    }
   }
 }
 </script>
